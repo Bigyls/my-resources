bb10716121cd780002e39045c496a3d29efcf64f
Bigyls <74175999+Bigyls@users.noreply.github.com>
2025-07-01 21:30:42 +0200

    refactor(logs): use the exegol default logs

--- setup/load_user_setup.sh
+++ setup/load_user_setup.sh
@@ -2,15 +2,10 @@
 
 START_TIME=$(date +%s)
 
-LOG_FILE="/.exegol/setup.log"
-ERROR_LOG="/workspace/error.log"
+ERROR_LOG_FILE="/workspace/error.log"
 SCRIPTS_DIR="$(dirname "$0")/scripts"
 
 source "$SCRIPTS_DIR/helpers/logs.sh"
-
-rm -rf "$LOG_FILE" "$ERROR_LOG"
-
-log INFO "==== Setup started at $(date) ====" > "$LOG_FILE"
 
 pids=()
 tmp_script_logfiles=()
@@ -29,7 +24,7 @@ scripts=("$SCRIPTS_DIR"/*.sh)
 shopt -u nullglob
 
 if [ ${#scripts[@]} -eq 0 ]; then
-  log ERROR "No scripts found in $SCRIPTS_DIR" >> "$LOG_FILE"
+  log ERROR "No scripts found in $SCRIPTS_DIR"
   exit 0
 fi
 
@@ -37,7 +32,7 @@ for script in "${scripts[@]}"; do
   tmp_logfile=$(mktemp /tmp/setup.log.XXXXXX)
   tmp_script_logfiles+=("$tmp_logfile")
   script_names+=("$script")
-  log INFO "Executing $(basename "$script"). See logs in $tmp_logfile"
+  log INFO "Executing $(basename "$script") ..."
   (
     log START "$script"
     bash "$script"
@@ -59,21 +54,15 @@ for i in "${!pids[@]}"; do
   script=${script_names[$i]}
   tmp_logfile=${tmp_script_logfiles[$i]}
   if ! wait "$pid"; then
-    log ERROR "An error occurred while executing $(basename "$script"). Check "$LOG_FILE" for details." >> "$ERROR_FILE"
-    cat "$tmp_logfile" >> "$ERROR_LOG"
-    echo -e "\n------\n" >> "$ERROR_LOG"
+    log ERROR "An error occurred while executing $(basename "$script"). Check "$ERROR_LOG_FILE" for details."
+    cat "$tmp_logfile" >> "$ERROR_LOG_FILE"
+    echo -e "\n------\n" >> "$ERROR_LOG_FILE"
   fi
 done
 
 # Concatenate logs in order
 for tmp_logfile in "${tmp_script_logfiles[@]}"; do
-  cat "$tmp_logfile" >> "$LOG_FILE"
-  echo -e "\n------\n" >> "$LOG_FILE"
+  cat "$tmp_logfile"
+  echo -e "\n------\n"
   rm -f "$tmp_logfile"
 done
-
-END_TIME=$(date +%s)
-ELAPSED=$((END_TIME - START_TIME))
-
-log INFO "==== Setup finished at $(date) ====" >> "$LOG_FILE"
-log INFO "==== Elapsed time: ${ELAPSED} seconds ====" >> "$LOG_FILE"
--- setup/scripts/helpers/logs.sh
+++ setup/scripts/helpers/logs.sh
@@ -2,10 +2,10 @@ log() {
     level="$1"
     shift
     case "$level" in
-        "START") echo "[START] [$(date +'%F %T')] $*" ;;
-        "SUCCESS") echo "[SUCCESS] [$(date +'%F %T')] $*" ;;
-        "ERROR") echo "[ERROR] [$(date +'%F %T')] $*" >&2 ;;
-        "END") echo "[END] [$(date +'%F %T')] $*" ;;
-        *) echo "[INFO] [$(date +'%F %T')] $*" ;;
+        "START") echo "[START] [$(date +'%F %T')] [BIGYLS MY-RESOURCES] $*" ;;
+        "SUCCESS") echo "[SUCCESS] [$(date +'%F %T')] [BIGYLS MY-RESOURCES] $*" ;;
+        "ERROR") echo "[ERROR] [$(date +'%F %T')] [BIGYLS MY-RESOURCES] $*" >&2 ;;
+        "END") echo "[END] [$(date +'%F %T')] [BIGYLS MY-RESOURCES] $*" ;;
+        *) echo "[INFO] [$(date +'%F %T')] [BIGYLS MY-RESOURCES] $*" ;;
     esac
 }
