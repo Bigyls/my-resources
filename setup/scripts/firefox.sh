b831ffb3ca37b6b2f968c4566e0a13c301d86c70
Bigyls <74175999+Bigyls@users.noreply.github.com>
2025-07-02 16:45:48 +0200

    feat(firefox): add extensions and  change declaration method to choose area

--- setup/scripts/firefox.sh
+++ setup/scripts/firefox.sh
@@ -7,25 +7,34 @@ POLICY_FILENAME="policies.json"
 POLICY_PATH="/usr/lib/firefox-esr/distribution"
 TEMPLATE_PATH="$(dirname "$0")/../firefox/${POLICY_FILENAME}.template"
 
-NAMES=(
-    "pwnfox"
-    "uaswitcher"
-    "wappalyzer"
-    "multi-account-containers"
-    "multi-url-opener"
-    "bitwarden-password-manager"
-    "hacktools"
-    "shodan-addon"
-    "ghunt-companion"
-    "ublock-origin"
-    "traduzir-paginas-web"
-    "cookie-quick-manager"
-    "easy-xss"
-    "web-developer"
-    "penetration-testing-kit"
-    "firefox-hackbar"
-    "what-cms-is-this"
-    "dotgit"
+declare -A EXTENSIONS=(
+    ["pwnfox"]="navbar"
+    ["uaswitcher"]="menupanel"
+    ["wappalyzer"]="navbar"
+    ["bitwarden-password-manager"]="navbar"
+    ["hacktools"]="navbar"
+    ["shodan-addon"]="menupanel"
+    ["ghunt-companion"]="menupanel"
+    ["ublock-origin"]="menupanel"
+    ["traduzir-paginas-web"]="navbar"
+    ["cookie-quick-manager"]="menupanel"
+    ["easy-xss"]="menupanel"
+    ["web-developer"]="menupanel"
+    ["penetration-testing-kit"]="menupanel"
+    ["firefox-hackbar"]="menupanel"
+    ["what-cms-is-this"]="menupanel"
+    ["dotgit"]="navbar"
+    ["matte-dark-red-theme"]="menupanel"
+    ["trufflehog"]="menupanel"
+    ["temp-mail"]="menupanel"
+    ["hunterio"]="menupanel"
+    ["webrtc-protect"]="menupanel"
+    ["findsomething"]="menupanel"
+    ["link-gopher"]="menupanel"
+    ["open-multiple-urls"]="menupanel"
+    ["endpointer"]="menupanel"
+    ["yeswehack-vdp-finder"]="menupanel"
+    ["s3bucketlist"]="menupanel"
 )
 
 get_extension_id() {
@@ -52,15 +61,15 @@ log INFO "Generating the Firefox policy"
     TMP_JSON=$(mktemp)
     cp "$TEMPLATE_PATH" "$TMP_JSON"
 
-    for EXT_NAME in "${NAMES[@]}"; do
+    for EXT_NAME in "${!EXTENSIONS[@]}"; do
         EXT_GUID=$(get_extension_id "$EXT_NAME") || continue
-        jq --arg guid "$EXT_GUID" --arg name "$EXT_NAME" \
-            '.policies.ExtensionSettings[$guid] = {"installation_mode":"force_installed", "temporarily_allow_weak_signatures": true, "private_browsing": true, "default_area": "navbar", "install_url":("https://addons.mozilla.org/firefox/downloads/latest/" + $name + "/latest.xpi")}' \
+        DEFAULT_AREA="${EXTENSIONS[$EXT_NAME]}"
+        jq --arg guid "$EXT_GUID" --arg name "$EXT_NAME" --arg area "$DEFAULT_AREA" \
+            '.policies.ExtensionSettings[$guid] = {"installation_mode":"force_installed", "temporarily_allow_weak_signatures": true, "private_browsing": true, "default_area": $area, "install_url": ("https://addons.mozilla.org/firefox/downloads/latest/" + $name + "/latest.xpi")}' \
             "$TMP_JSON" > "${TMP_JSON}.tmp" && mv "${TMP_JSON}.tmp" "$TMP_JSON"
     done
 
-log INFO "NAMES contains ${#NAMES[@]} elements"
-    cp "$TMP_JSON" "${POLICY_PATH}/${POLICY_FILENAME}"
-    rm "$TMP_JSON"
+cp "$TMP_JSON" "${POLICY_PATH}/${POLICY_FILENAME}"
+rm "$TMP_JSON"
 
 log INFO "Policy written to ${POLICY_PATH}/${POLICY_FILENAME}"
