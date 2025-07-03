1b1e2a53ab41988b4f433d96706b54ad31f61151
Bigyls <74175999+Bigyls@users.noreply.github.com>
2025-07-03 08:46:33 +0200

    feat(nxc): add wrapper to history

--- setup/scripts/nxc.sh
+++ setup/scripts/nxc.sh
@@ -5,5 +5,9 @@ source "$(dirname "$0")/helpers/logs.sh"
 
 log INFO "Creating ~/.nxc directory..."
     mkdir -p ~/.nxc
+
 log INFO "Copying nxc.conf..."
     cp "$(dirname "$0")/../nxc/nxc.conf" ~/.nxc/nxc.conf
+
+log INFO "Installing NetExecWrapper4ExegolHistory..."
+    curl -sSL https://raw.githubusercontent.com/Frozenka/nxcwrap/refs/heads/main/install_nxcwraper.sh | bash
